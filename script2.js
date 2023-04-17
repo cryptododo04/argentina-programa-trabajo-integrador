cb8ad1c4fdb354756a9083e0fe8936b499b75a60
santi cuda <santiagoacuda@gmail.com>
2023-04-16 21:40:03 -0300

    agregada nueva seccion de paquetes  con carousel de tarjetas por cada paquete

--- script2.js
+++ script2.js
@@ -16,6 +16,31 @@ document.addEventListener("DOMContentLoaded", function() {
     typeWriter();
 });
 
+/* card carrousel*/
+
+let carouselWidth = $('.carousel-inner')[0].scrollWidth;
+
+let cardWidth = $('.carousel-item').width();
+
+let scrollPosition = 0;
+
+$(".carousel-control-next").on("click", function () {
+  if (scrollPosition < (carouselWidth - cardWidth * 4)) { //check if you can go any further
+    scrollPosition += cardWidth;  //update scroll position
+    $(".carousel-inner").animate({ scrollLeft: scrollPosition },600); //scroll left
+  }
+});
+
+$(".carousel-control-prev").on("click", function () {
+  if (scrollPosition > 0) {
+    scrollPosition -= cardWidth;
+    $(".carousel-inner").animate(
+      { scrollLeft: scrollPosition },
+      600
+    );
+  }
+});
+
 
 
 //script del formulario de contrato, 
